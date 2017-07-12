4713d10bd17cb3e5c7ee9ace3dd441ca9f82672a
Didi Yudha Perwira <didiyudha@gmail.com>
2017-07-12 20:33:03 +0700

    Adding socketio to the app and handling connect and disconnect user

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 
 const PORT = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`);
 });
