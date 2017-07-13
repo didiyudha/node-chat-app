d62364d86347b673c7433e8700a3078830b52742
Didi Yudha Perwira <didiyudha@gmail.com>
2017-07-13 11:56:38 +0700

    emit new message on create message

--- server/server.js
+++ server/server.js
@@ -16,18 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'server',
-    to: 'client',
-    createdAt: new Date().getTime()
-  });
-
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 
   socket.on('createMessage', (newMessage) => {
     console.log('Create a message: ', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 });
 
