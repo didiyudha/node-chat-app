bd1c1be731b2e30d776511f79ea86b0b0dc7fc74
Didi Yudha Perwira <didiyudha@gmail.com>
2017-07-13 18:14:07 +0700

    Adding jQuery libraries and create form and send the message to the server

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,33 @@
 var socket = io();
 
-socket.on('connect', function() {
+// Client connected to the server
+socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('onConnected', function(msg) {
-  console.log('onConnected: ', msg);
+// Client disconnected to the server
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newJoin', function(msg) {
-  console.log('newJoin: ', msg);
+// Client receive message from server
+// client print the message and binding it int <ul> component
+socket.on('newMessage', function (message) {
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}-${message.text}`);
+  jQuery('#messages').append(li);
 });
 
-socket.on('disconnect', function() {
-  console.log('Disconnected from the server');
-});
+// Handle input from user form
+// grab the message and send it to the server
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  var text = jQuery('[name=message]').val();
+  jQuery('[name=message]').val('');
+  socket.emit('createMessage', {
+    from: 'User',
+    text: text
+  }, function() {
 
-socket.on('newMessage', function(message) {
-  console.log('New message: ', message);
-});
-
-socket.emit('createMessage', {
-  from: 'Julia',
-  text: 'Hi All'
-}, function(data) {
-  console.log('Got it.', data);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -1,45 +1,46 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
-var { generateMessage } = require('./utils/message');
-
-const publicPath = path.join(__dirname,'../public');
-
-const PORT = process.env.PORT || 3000;
-
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+// Tell the express middleware to use static assets
+// and where the location of relative path is
 app.use(express.static(publicPath));
 
+// Waiting for the client connected to the server
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('onConnected', generateMessage('Admin', 'Welcome to chat app'));
+  // Initial message from server to the client when connected
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newJoin', generateMessage('Admin', 'New user join'));
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  // Server broadcast message to the client 
+  // and tell the client that there's new client connected
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  
+  // Server receive a message from client
+  // server send message back to all clients that connected
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    // io.emit will send message to the clients that connected
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // acknowledgement from server
+    callback('This is from server');
   });
 
-  socket.on('createMessage', (newMessage, callback) => {
-    console.log('Create a message: ', newMessage);
-
-    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-    callback('This is from server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: newMessage.from,
-    //   text: newMessage.text,
-    //   createdAt: new Date().getTime()
-    // });
-
+  // Detect if there's a client disconnected to server and logged it
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
   });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is up on port ${PORT}`);
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
